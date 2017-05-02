106d493b866879eeb63afc505d39d647ce3e7437
Billy Charlton <billy@okbecause.com>
2017-05-02 12:52:54 -0700

    add import-from-shapefile script

--- cmp/create_geojson_from_postgis.sql
+++ cmp/create_geojson_from_postgis.sql
@@ -4,6 +4,6 @@ CREATE VIEW cmp.json_segments AS
 SELECT *,ST_AsGeoJSON(cmp_segments.geom) as geometry
   FROM cmp.cmp_segments;
 
-ALTER VIEW cmp.json_segments OWNER to staff;
+ALTER VIEW cmp.json_segments OWNER to anon;
 GRANT SELECT ON TABLE cmp.json_segments TO anon;
 
