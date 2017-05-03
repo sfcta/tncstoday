78a75c2a6476477b0b6e865840337f2693698f7d
Billy Charlton <billy@okbecause.com>
2017-05-03 15:21:37 -0700

    use offsets instead of trying to do two charts at once

--- cmp/create_geojson_from_postgis.sql
+++
@@ -1,9 +0,0 @@
--- PostGIS can convert any geometry to a GeoJSON view
-DROP VIEW cmp.json_segments;
-CREATE VIEW cmp.json_segments AS
-SELECT *,ST_AsGeoJSON(cmp_segments.geom) as geometry
-  FROM cmp.cmp_segments;
-
-ALTER VIEW cmp.json_segments OWNER to anon;
-GRANT SELECT ON TABLE cmp.json_segments TO anon;
-
