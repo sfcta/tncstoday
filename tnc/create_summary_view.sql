272efcf0e07824b95f95f2422673eff411ada0c6
Billy Charlton <billy@okbecause.com>
2017-06-01 15:59:08 -0700

    Update avail_trips/accpt_trips column names to new: pickups/dropoffs

--- tnc/create_summary_view.sql
+++ tnc/create_summary_view.sql
@@ -1,8 +1,8 @@
 -- PostGIS can convert any geometry to a GeoJSON view
-DROP VIEW tnc.taz_total;
-CREATE VIEW tnc.taz_total (taz,day_of_week,avail_trips,accpt_trips) AS 
+-- DROP VIEW tnc.taz_total;
+CREATE VIEW tnc.taz_total (taz, day_of_week, dropoffs, pickups) AS 
 SELECT 
-  taz,day_of_week,SUM(avail_trips),SUM(accpt_trips)
+  taz,day_of_week,SUM(dropoffs),SUM(pickups)
 FROM 
   tnc.tnc_trip_stats
 GROUP BY day_of_week,taz
@@ -10,4 +10,3 @@ ORDER BY taz,day_of_week;
 
 ALTER VIEW tnc.taz_total OWNER to anon;
 GRANT SELECT ON TABLE tnc.taz_total TO anon;
-
