51ab42faf4ac4880111d9d66b4acc2b191fffeaf
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-28 13:08:53 +0800

    Delete pgsql directory

--- pgsql/create_geojson_view.sql
+++
@@ -1,8 +0,0 @@
--- PostGIS can convert any geometry to a GeoJSON view
-CREATE OR REPLACE VIEW api.taz_boundaries AS
-SELECT *,
-       st_asgeojson(tnc.taz_boundaries.geom) AS geometry
-FROM tnc.taz_boundaries;
-
-ALTER VIEW api.taz_boundaries OWNER to postgres;
-GRANT SELECT ON TABLE api.taz_boundaries TO anon;
